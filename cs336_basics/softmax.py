54a9d5fb0f3a2acd4a53576d4770f7bbdbc070e5
Runyu Wang <527159991@qq.com>
2026-03-12 19:04:40 +0800

    在softmax函数中添加注释以解释上溢问题的解决方案

--- cs336_basics/softmax.py
+++ cs336_basics/softmax.py
@@ -5,6 +5,7 @@ def softmax(input: torch.Tensor, dim: int):
     # batch, token_num, hidden_size
     # max
     (max_val, _) = input.max(dim=dim, keepdim=True)
+    # 解决上溢问题
     input = input - max_val
     exp_input = torch.exp(input)
     exp_sum = torch.sum(exp_input, dim=dim, keepdim=True)
